9d79a1686fd08aba4fcb588171334766a6095f52
s.ovchinnikov <kazatca@gmail.com>
2020-04-19 19:33:07 +1000

    unfinished

---
+++ src/components/MobileHeader/components/LangSelector/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+import * as b_ from 'b_'
+
+
+const b = b_.with('lang-selector');
+
+const LangSelector = () => 
+  <div>
+    
+  </div>
+export default LangSelector;

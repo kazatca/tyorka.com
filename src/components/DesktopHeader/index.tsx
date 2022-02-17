7a11edeaff91bc204e16488bef6ee594e92e2c81
s.ovchinnikov <kazatca@gmail.com>
2022-02-17 21:48:03 +1000

    feat: new logo size

--- src/components/DesktopHeader/index.tsx
+++ src/components/DesktopHeader/index.tsx
@@ -1,57 +1,68 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
 import * as b_ from 'b_'
-import { useTranslate } from '../../hooks/translate';
-import CornerMenu from '../CornerMenu';
+import { useTranslate } from '../../hooks/translate'
+import CornerMenu from '../CornerMenu'
 
-import './index.scss';
+import './index.scss'
 
-import logo from "./static/logo_377.png";
+import logo from './static/logo_377.png'
 
-const b = b_.with('desktop-header');
+const b = b_.with('desktop-header')
 
 interface Props {
   fixed?: boolean
 }
 
-const headerHeightMax = 400;
-const headerHeightMin = 140;
+const headerHeightMax = 400
+const headerHeightMin = 140
 
-const DesktopHeader: React.FC<Props> = ({fixed}) => {
-  const logoRef = React.useRef<HTMLImageElement>(null);
-  
+const DesktopHeader: React.FC<Props> = ({ fixed }) => {
+  const logoRef = React.useRef<HTMLImageElement>(null)
+
   const { t } = useTranslate()
 
   const resizeHeader = () => {
-    const top = window.scrollY;
-    if(logoRef.current) {
-      logoRef.current.style.width = Math.max(headerHeightMax - top, headerHeightMin) + 'px'
+    const top = window.scrollY
+    if (logoRef.current) {
+      logoRef.current.style.width =
+        Math.max(headerHeightMax - top, headerHeightMin) + 'px'
     }
   }
 
   React.useEffect(() => {
-    if(typeof window !== 'undefined' && fixed){
-      window.addEventListener('scroll', resizeHeader);
+    if (typeof window !== 'undefined' && fixed) {
+      window.addEventListener('scroll', resizeHeader)
     }
     return () => {
-      window.removeEventListener('scroll', resizeHeader);  
+      window.removeEventListener('scroll', resizeHeader)
     }
   })
 
   return (
-    <section className={b({fixed})}>
+    <section className={b({ fixed })}>
       <CornerMenu />
-      <div className={b("menu")}>
-        <Link to='/' activeClassName='active'>{t('Main page')}</Link>
-        <Link partiallyActive to='/about' activeClassName='active'>{t('About page')}</Link>
+      <div className={b('menu')}>
+        <Link to="/" activeClassName="active">
+          {t('Main page')}
+        </Link>
+        <Link partiallyActive to="/about" activeClassName="active">
+          {t('About page')}
+        </Link>
       </div>
-      <Link to="/"><img className={b("logo")} src={logo} ref={logoRef}/></Link>
-      <div className={b("menu")}>
-        <Link partiallyActive to='/blog' activeClassName='active'>{t('Blog page')}</Link>
-        <Link partiallyActive to='/shop' activeClassName='active'>{t('Shop page')}</Link>
+      <Link to="/" className={b('logo-link')}>
+        <img className={b('logo')} src={logo} ref={logoRef} />
+      </Link>
+      <div className={b('menu')}>
+        <Link partiallyActive to="/blog" activeClassName="active">
+          {t('Blog page')}
+        </Link>
+        <Link partiallyActive to="/shop" activeClassName="active">
+          {t('Shop page')}
+        </Link>
       </div>
     </section>
-  );
+  )
 }
 
 export default DesktopHeader

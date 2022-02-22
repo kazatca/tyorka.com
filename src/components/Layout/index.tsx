92babd7e9da29debed248c1f0ca72ee93a77796e
s.ovchinnikov <s.ovchinnikov@unitedtraders.com>
2022-02-22 20:56:42 +1000

    feat: translate title

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -8,6 +8,7 @@ import { Socials } from '../Socials'
 import { LanguageSelector } from '../LanguageSelector'
 
 import './index.scss'
+import { useTranslate } from '../../hooks/translate'
 
 const b = b_.with('content')
 
@@ -17,22 +18,25 @@ interface Props {
   noHeaderOnDesktop?: boolean
 }
 
-export const Layout: React.FC<Props> = ({ children, noHeaderOnDesktop }) => (
-  <>
-    <Helmet title="Тёрка">
-      <html lang={process.env.GATSBY_LNG} />
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1, user-scalable=no"
-      />
-    </Helmet>
-    <div className={b({ 'fixed-menu': fixedMenu })}>
-      {!noHeaderOnDesktop && <DesktopHeader fixed={fixedMenu} />}
-      <MobileHeader />
-      {children}
-      <Socials />
-      <LanguageSelector />
-      <ToTopButton />
-    </div>
-  </>
-)
+export const Layout: React.FC<Props> = ({ children, noHeaderOnDesktop }) => {
+  const { t } = useTranslate()
+  return (
+    <>
+      <Helmet title={t('Tyorka')}>
+        <html lang={process.env.GATSBY_LNG} />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, user-scalable=no"
+        />
+      </Helmet>
+      <div className={b({ 'fixed-menu': fixedMenu })}>
+        {!noHeaderOnDesktop && <DesktopHeader fixed={fixedMenu} />}
+        <MobileHeader />
+        {children}
+        <Socials />
+        <LanguageSelector />
+        <ToTopButton />
+      </div>
+    </>
+  )
+}

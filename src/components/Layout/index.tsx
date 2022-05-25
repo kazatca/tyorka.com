ca90d64c1c44efe72b01b6a1e9d2cbb04b759514
s.ovchinnikov <s.ovchinnikov@unitedtraders.com>
2022-05-25 17:09:37 +0400

    feat: added SEO tags

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -8,6 +8,7 @@ import { Socials } from '../Socials'
 import { LanguageSelector } from '../LanguageSelector'
 import { useTranslate } from '../../hooks/translate'
 import favicon from './static/favicon.svg'
+import image from './static/image.svg'
 
 import './index.scss'
 
@@ -30,6 +31,20 @@ export const Layout: React.FC<Props> = ({ children, noHeaderOnDesktop }) => {
           content="width=device-width, initial-scale=1, user-scalable=no"
         />
         <link rel="icon" href={favicon}/>
+        <meta name="title" content={t('Tyorka')} />
+        <meta name="description" content={t('site-description')} />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://tyorka.com/" />
+        <meta property="og:title" content={t('Tyorka')} />
+        <meta property="og:description" content={t('site-description')} />
+        <meta property="og:image" content={image} />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://tyorka.com/" />
+        <meta property="twitter:title" content={t('Tyorka')} />
+        <meta property="twitter:description" content={t('site-description')} />
+        <meta property="twitter:image" content={image} />
       </Helmet>
       <div className={b({ 'fixed-menu': fixedMenu })}>
         {!noHeaderOnDesktop && <DesktopHeader fixed={fixedMenu} />}

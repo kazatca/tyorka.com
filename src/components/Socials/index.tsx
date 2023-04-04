290d2380ad1b79753ce688f4d2b6e57b6ebdec4b
slava.ovchinnikov <viacheslav.ovchinnikov@fyve.com>
2023-04-05 00:03:03 +0400

    feat: fixed links at footer

--- src/components/Socials/index.tsx
+++ src/components/Socials/index.tsx
@@ -3,9 +3,8 @@ import * as b_ from 'b_'
 import { Link } from './Link'
 import './index.scss'
 
-import insta from './static/insta.svg'
-import etsy from './static/etsy.png'
-import fb from './static/fb.png'
+import insta from './static/insta.png'
+import tg from './static/tg.png'
 import vk from './static/vk.png'
 
 const b = b_.with('socials')
@@ -13,8 +12,7 @@ const b = b_.with('socials')
 export const Socials = () => (
   <section className={b()}>
     <Link href="https://www.instagram.com/tyorka" icon={insta} alt="insta" />
-    <Link href="https://www.etsy.com/shop/tyorka" icon={etsy} alt="etsy" />
-    <Link href="https://www.facebook.com/tyorka" icon={fb} alt="fb" />
-    <Link href="https://vk.com/tyorkaa" icon={vk} alt="fb" />
+    <Link href="https://t.me/kaptyorka" icon={tg} alt="tg" />
+    <Link href="https://vk.com/tyorkaa" icon={vk} alt="vk" />
   </section>
 )
